bc92997724ffc8906a02a35f886b24a5b09298f9
kyang12 <kyang12@ncsu.edu>
2017-10-17 20:42:22 -0400

    DRY MVC

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,13 +11,17 @@ class MoviesController < ApplicationController
   end
 
   def new
-    # default: render 'new' template
+    @movie = Movie.new
   end
 
   def create
-    @movie = Movie.create!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      flash[:notice] = "#{@movie.title} was successfully created."
+      redirect_to movies_path
+    else
+      render 'new' # note, 'new' template can access @movie's field values!
+    end
   end
 
   def edit
@@ -25,12 +29,15 @@ class MoviesController < ApplicationController
   end
 
   def update
-    @movie = Movie.find(params[:id])
-    @movie.update_attributes!(movie_params)
-    flash[:notice] = "#{@movie.title} was successfully updated."
-    respond_to do |client_wants|
-      client_wants.html {  redirect_to movie_path(@movie)  } # as before
-      client_wants.xml  {  render :xml => @movie.to_xml    }
+    @movie = Movie.find params[:id]
+    if @movie.update_attributes(movie_params)
+      flash[:notice] = "#{@movie.title} was successfully updated."
+      respond_to do |client_wants|
+        client_wants.html {  redirect_to movie_path(@movie)  } # as before
+        client_wants.xml  {  render :xml => @movie.to_xml    }
+      end
+    else
+      render 'edit' # note, 'edit' template can access @movie's field values!
     end
   end
 
